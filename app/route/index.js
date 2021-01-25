e0a7e64a75da1a6141548326b624340414476c08
Richard Nava <richard.ig.nava@gmail.com>
2021-01-24 21:27:23 -0600

    Bring in express and require

--- app/route/index.js
+++ app/route/index.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const router  = express.Router();
+import express from 'express';
+import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
+import cons from 'consolidate';
+import { fileURLToPath } from 'url';
+import require from 'requirejs'
+
+const path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = 5000;
+
 //login page
-router.get('/', (req,res)=>{
+app.get('/', (req,res)=>{
     res.render('welcome');
 })
 //register page
-router.get('/register', (req,res)=>{
+app.get('/register', (req,res)=>{
     res.render('register');
 })
 
-module.exports = router; 
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))
